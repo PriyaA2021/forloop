e1da6f24b47f8c198c4810acc0afc9bec857f7ae
Sripriya Sundararaman <spreiyah@yahoo.com>
2021-06-11 16:24:00 +1200

    Add project files.

---
+++ forloop/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace forloop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i,n;
+            Console.WriteLine("Enter number until which you want the numbers to be displayed");
+            n = Convert.ToInt32(Console.ReadLine());
+            for(i=0; i<n; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
